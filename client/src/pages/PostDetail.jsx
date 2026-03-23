aa0b87f7e63ef4759cd677a7a2a0c1635c0dbcad
Omoju-Mayowa <omojumayowa@gmail.com>
2026-03-23 13:35:01 +0100

    refactor: added temporary admin rights — not logical will be fixed up later

--- client/src/pages/PostDetail.jsx
+++ client/src/pages/PostDetail.jsx
@@ -116,8 +116,7 @@ const PostDetail = () => {
 
   const imageUrl = resolveUrl(post?.thumbnail)
   const videoUrl = resolveUrl(post?.videoUrl)
-  const isOwner  = currentUser?.id === post?.creator?._id?.toString() || currentUser?.id === import.meta.env.ADMIN
-  console.log(import.meta.env.VITE_ADMIN)
+  const isOwner  = currentUser?.id === post?.creator?._id?.toString() || currentUser?.id === import.meta.env.VITE_ADMIN
 
   const renderSection = (section) => {
     switch (section) {
